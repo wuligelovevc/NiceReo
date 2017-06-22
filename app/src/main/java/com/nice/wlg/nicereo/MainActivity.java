79dfa7e5919ec6b5aba029f0797d14d650b411a0
wuligelovevc <wuligelovevc@gmail.com>
2017-06-22 22:28:49 +0800

    use retrofit

--- app/src/main/java/com/nice/wlg/nicereo/MainActivity.java
+++ app/src/main/java/com/nice/wlg/nicereo/MainActivity.java
@@ -8,6 +8,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nice.wlg.nicereo.api.InfoService;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -52,7 +63,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         switch (id) {
             case R.id.testbtn:
-
+                getNetWorkText();
+                break;
         }
     }
+
+    private void getNetWorkText() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:5000/")
+                .build();
+        InfoService service = retrofit.create(InfoService.class);
+        Call<ResponseBody> result = service.getInitText("hi");
+        result.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Toast.makeText(MainActivity.this, "s: " + response.body().string(), Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "f: " + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/nice/wlg/nicereo/api/InfoService.java
+++ app/src/main/java/com/nice/wlg/nicereo/api/InfoService.java
@@ -1,8 +1,16 @@
 package com.nice.wlg.nicereo.api;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 /**
  * Created by Administrator on 2017/6/22.
  */
 
-public class InfoService {
+public interface InfoService {
+
+    @GET("profile/{user}")
+    Call<ResponseBody> getInitText(@Path("user") String user);
 }
